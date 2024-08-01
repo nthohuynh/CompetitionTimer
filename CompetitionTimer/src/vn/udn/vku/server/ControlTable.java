0ef35d3135663c5ff65f46c02fc59473b6ff5bb5
Huynh Ngoc Tho <ntho.huynh@gmail.com>
2024-08-01 09:50:17 +0700

    s

--- CompetitionTimer/src/vn/udn/vku/server/ControlTable.java
+++ CompetitionTimer/src/vn/udn/vku/server/ControlTable.java
@@ -60,7 +60,7 @@ public class ControlTable extends JPanel {
 		JButton jbtStopQuestion = new JButton("Stop");
 		pnQuestionTimer.add(jbtStopQuestion);
 		add(pnQuestionTimer);
-		//Test
+		//Test dsd
 	}
 	public static void main(String agrs[]) {
 		JFrame fr = new JFrame();
