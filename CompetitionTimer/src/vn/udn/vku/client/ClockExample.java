2e72bc7af307b39172290e1ee894c98331260f06
Huynh Ngoc Tho <ntho.huynh@gmail.com>
2024-08-01 19:35:06 +0700

    n

--- CompetitionTimer/src/vn/udn/vku/client/ClockExample.java
+++ CompetitionTimer/src/vn/udn/vku/client/ClockExample.java
@@ -4,134 +4,207 @@ import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 
- 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.*;
 
-public class ClockExample extends JFrame {
+class UserClock implements Runnable {
 	private JLabel labelClock;
-	 
-    public ClockExample() {
-        setTitle("Đồng hồ trong Java Swing");
-        labelClock = new JLabel();
-        labelClock.setBounds(10, 10, 80, 60);
-        labelClock.setFont(new Font("Sans Serif", Font.PLAIN, 30));
-        
-        add(labelClock);
-        setSize(100, 80);
-       
-        setType(javax.swing.JFrame.Type.UTILITY); // don't display icon in tool bar in windows
-        setUndecorated(true); // <-- the title bar is removed here
+	int clockKind;
+	int presentationMinutes;
+	JFrame jFrame;
+	boolean chk = true;
+	int studentSeconds = presentationMinutes*60;
+      //int minute = minutes;
+    int second = 60;
+    Thread Objth;
+	public UserClock(JFrame jFrame, int clockKind, int presentationMinutes) {
+		super();
+		this.jFrame = jFrame;
+		this.clockKind = clockKind;
+		this.presentationMinutes = presentationMinutes - 1;
+		
+	}
 
-        setLayout(null);
-        
-    //  dóng chương trình khi đóng của sổ
-        addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(1);
-            }
-        });
-        setVisible(true);
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); 
-        setLocation(dim.width - this.getSize().width, this.getSize().height);
-        
-        setAlwaysOnTop(true);
-        
-        int minutes = 5;
-        // thiết lập lại đồng hồ sau mỗi 1 giây
-        int seconds = minutes*60;
-        //int minute = minutes;
-        int second = 60;
-        if(!SystemTray.isSupported()){
-            System.out.println("System tray is not supported !!! ");
-            return ;
-        }
-        
-        
-        
-        
-        //get the systemTray of the system
-        SystemTray systemTray = SystemTray.getSystemTray();
+	public void create()
+ 	{
+		Objth = new Thread(this);
+    	Objth.start();
+  	}
 
-        //get default toolkit
-        //Toolkit toolkit = Toolkit.getDefaultToolkit();
-        //get image 
-        //Toolkit.getDefaultToolkit().getImage("src/resources/busylogo.jpg");
-        Image image = Toolkit.getDefaultToolkit().getImage("src/images/icon.JPG");
+	public void stop()
+ 	{
+    	chk = false;
+  	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		JFrame fr = new JFrame();
 
-        //popupmenu
-        PopupMenu trayPopupMenu = new PopupMenu();
+	
+		labelClock = new JLabel();
+		labelClock.setBounds(10, 10, 80, 60);
+		labelClock.setFont(new Font("Sans Serif", Font.PLAIN, 30));
+	
+		
+		jFrame.setSize(100, 80);
 
-        //1t menuitem for popupmenu
-        MenuItem action = new MenuItem("Action");
-        action.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFrame fr  = new JFrame();
-                fr.setSize(400,80);
-                
-                fr.setLayout(new FlowLayout());
-                JLabel jlbServerIP = new JLabel("Server IP Address ");
-                JTextField jtfServerIP = new JTextField(20);
-                fr.add(jlbServerIP);
-                fr.add(jtfServerIP);
-                fr.setLocationRelativeTo(null);
-                //fr.setDefaultCloseOperation(EXIT_ON_CLOSE);
-                fr.setVisible(true);
-              
-                
-            	//JOptionPane.showMessageDialog(null, "Action Clicked");          
-            }
-        });     
-        trayPopupMenu.add(action);
+		jFrame.setType(javax.swing.JFrame.Type.UTILITY); // don't display icon in tool bar in windows
+		jFrame.setUndecorated(true); // <-- the title bar is removed here
 
-        //2nd menuitem of popupmenu
-        MenuItem close = new MenuItem("Close");
-        close.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);             
-            }
-        });
-        trayPopupMenu.add(close);
+		jFrame.setLayout(null);
+	
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		
+		if (clockKind == 1) jFrame.setLocation(dim.width - jFrame.getSize().width , jFrame.getSize().height+10);
+		else {
+			labelClock.setBackground(Color.lightGray);
+			labelClock.setOpaque(true);
+			jFrame.setLocation(dim.width - jFrame.getSize().width , dim.height - jFrame.getSize().height - 100);
+		}
+		jFrame.add(labelClock);
+		jFrame.setAlwaysOnTop(true);
+		jFrame.setVisible(true);
+		
+		
+		try {
+	            while (chk) {
+	                if (second > 0) second --;
+	                else {
+	                	second = 60;
+	                	presentationMinutes = presentationMinutes - 1;
+	                }
+	                labelClock.setText(((presentationMinutes  <= 9)? ("0" + presentationMinutes):presentationMinutes) + ":" + ((second <= 9)? "0"+second: second));
+	                Thread.sleep(1000);
+	            }
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+		
+	}
+	
+}
 
-        //setting tray icon
-        TrayIcon trayIcon = new TrayIcon(image, "SystemTray Demo", trayPopupMenu);
-        //adjust to default size as per system recommendation 
-        trayIcon.setImageAutoSize(true);
+public class ClockExample extends JFrame {
+	UserClock cl1;
+	UserClock cl2;
+	ObjectInputStream ois = null;
 
-        try{
-            systemTray.add(trayIcon);
-        }catch(AWTException awtException){
-            awtException.printStackTrace();
-        }
-              
-      
-        try {
-            while (true) {
-                if (second > 0) second --;
-                else {
-                	second = 60;
-                	minutes = minutes - 1;
-                }
-                labelClock.setText(((minutes  <= 9)? ("0" + minutes):minutes) + ":" + ((second <= 9)? "0"+second: second));
-                Thread.sleep(1000);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
- 
-    /**
-     * main
-     * 
-     * @param args
-     * @throws InterruptedException
-     */
-    public static void main(String[] args) {
-        new ClockExample();
-    }
+	public ClockExample() {
+		Thread t1 = new Thread(() -> {
+			try {
+				// Socket s = new Socket(args[0],Integer.parseInt(args[1]));
+				
+				Socket socket = new Socket("127.0.0.1", 8189);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				 BufferedReader kbd = new BufferedReader(new InputStreamReader(System.in));
+		         PrintWriter out = new PrintWriter(socket.getOutputStream(), true /* autoFlush */);
+		         System.out.println("Client");
+				// System.out.println(in.readLine());
+
+		         boolean done = false;
+		     
+					
+					System.out.println("lap");
+					while (!done){
+			            System.out.print("Client: ");
+					    //String fromClient = kbd.readLine();
+			            //out.println("Client: " + fromClient);
+			            String fromServer= in.readLine();
+						System.out.println(fromServer);
+						//if (fromServer.trim().equals("Server: BYE") || fromClient.equals("BYE"))  done = true;
+			        
+
+					
+					if (fromServer.equals("startPresentation")) {
+						JFrame jf = new JFrame();
+						cl1 = new UserClock(jf, 1, 5);
+						cl1.create();
+					} else if (fromServer.equals("stopPresentation")) {
+						cl1.stop();
+						JFrame jf2 = new JFrame();
+						cl2 = new UserClock(jf2, 2, 3);
+						cl2.create();
+					} else if (fromServer.equals("stopQuestion")) {
+						cl2.stop();
+					}
+					//ois.close();
+					}
+					
+				// socket.close();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		});
+		t1.start();
+		// run this frame when receiving a msg from server
+		System.out.println("chay tiep");
+		if (!SystemTray.isSupported()) {
+			System.out.println("System tray is not supported !!! ");
+			return;
+		}
+		// get the systemTray of the system
+		SystemTray systemTray = SystemTray.getSystemTray();
+		Image image = Toolkit.getDefaultToolkit().getImage("src/images/icon.JPG");
+		// popupmenu
+		PopupMenu trayPopupMenu = new PopupMenu();
+		// 1t menuitem for popupmenu
+		MenuItem action = new MenuItem("Action");
+		action.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame fr = new JFrame();
+				fr.setSize(400, 80);
+
+				fr.setLayout(new FlowLayout());
+				JLabel jlbServerIP = new JLabel("Server IP Address ");
+				JTextField jtfServerIP = new JTextField(20);
+				fr.add(jlbServerIP);
+				fr.add(jtfServerIP);
+				fr.setLocationRelativeTo(null);
+				// fr.setDefaultCloseOperation(EXIT_ON_CLOSE);
+				fr.setVisible(true);
+			}
+		});
+		trayPopupMenu.add(action);
+
+		// 2nd menuitem of popupmenu
+		MenuItem close = new MenuItem("Close");
+		close.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		trayPopupMenu.add(close);
+
+		// setting tray icon
+		TrayIcon trayIcon = new TrayIcon(image, "SystemTray Demo", trayPopupMenu);
+		// adjust to default size as per system recommendation
+		trayIcon.setImageAutoSize(true);
+
+		try {
+			systemTray.add(trayIcon);
+		} catch (AWTException awtException) {
+			awtException.printStackTrace();
+		}
+	}
+
+	/**
+	 * main
+	 * 
+	 * @param args
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) {
+		new ClockExample();
+	}
 }
--- CompetitionTimer/src/vn/udn/vku/server/ControlTable.java
+++ CompetitionTimer/src/vn/udn/vku/server/ControlTable.java
@@ -4,6 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -17,7 +24,25 @@ import javax.swing.border.TitledBorder;
 public class ControlTable extends JPanel {
 	JLabel jlbPresentationTime;
 	JLabel jlbQuestionTime;
-	public ControlTable() {
+	PrintWriter oos;
+	ObjectInputStream in;
+	
+	public PrintWriter getOut() {
+		return oos;
+	}
+	public void setOut(PrintWriter oos1) {
+		this.oos = oos1;
+	}
+	public ObjectInputStream getIn() {
+		return in;
+	}
+	public void setIn(ObjectInputStream in) {
+		this.in = in;
+	}
+	public ControlTable(PrintWriter out1, ObjectInputStream in1) {
+		this.oos = out1;
+		//this.in = in1;
+		
 		setLayout(new GridLayout(1,2));
 		JPanel pnPresentationTimer = new JPanel();
 		
@@ -41,9 +66,31 @@ public class ControlTable extends JPanel {
 		
 		JButton jbtStart = new JButton("Start");
 		pnPresentationTimer.add(jbtStart);
+		jbtStart.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+					oos.println("Hi Client3");
+					oos.println("startPresentation");
+				
+				System.out.println("server start presentation");
+			}
+		});
+		
 		
 		JButton jbtStop = new JButton("Stop");
 		pnPresentationTimer.add(jbtStop);
+		jbtStop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+					oos.println("stopPresentation");
+				
+			}
+		});
 				
 		add(pnPresentationTimer);
 		
@@ -55,16 +102,26 @@ public class ControlTable extends JPanel {
 		
 		jlbQuestionTime = new JLabel("Question time here");
 		pnQuestionTimer.add(jlbQuestionTime);
-		
+	
 		JButton jbtStopQuestion = new JButton("Stop");
 		pnQuestionTimer.add(jbtStopQuestion);
+		jbtStopQuestion.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+					oos.println("stopQuestion");
+				
+			}
+		});
+		
 		add(pnQuestionTimer);
 		//Test dsd  fsdd
 	}
 	public static void main(String agrs[]) {
 		JFrame fr = new JFrame();
 		fr.setSize(600,200);
-		fr.add(new ControlTable());
+		fr.add(new ControlTable(null,null));
 		fr.setVisible(true);
 	}
 }
